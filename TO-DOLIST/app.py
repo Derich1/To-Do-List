3aa39d9a25cf8d4f1e0fb1da4507588ca455af4b
Derich Rosario <38561150+Derich1@users.noreply.github.com>
2024-08-23 22:10:21 +0000

    correto

--- TO-DOLIST/app.py
+++ TO-DOLIST/app.py
@@ -11,7 +11,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-db = sqlite3.connect("sqlite:///project.db")
+db = sqlite3.connect("project.db")
 
 @app.after_request
 def after_request(response):
