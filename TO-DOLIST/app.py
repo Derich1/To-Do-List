7ccbc91e5b8370f631f6d598371e12b1dc95e867
Derich Rosario <38561150+Derich1@users.noreply.github.com>
2024-08-27 00:41:25 +0000

    isso

--- TO-DOLIST/app.py
+++ TO-DOLIST/app.py
@@ -4,6 +4,7 @@ import sqlite3
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
+from helpers import login_required
 
 app = Flask(__name__)
 
@@ -35,11 +36,11 @@ def login():
     if request.method == "POST":
         # Ensure username was submitted
         if not request.form.get("username"):
-            return 
+            return redirect("/")
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return 
+            return redirect("/")
 
         # Query database for username
         rows = db.execute(
@@ -50,7 +51,7 @@ def login():
         if len(rows) != 1 or not check_password_hash(
             rows[0]["hash"], request.form.get("password")
         ):
-            return
+            return redirect("/")
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
@@ -61,3 +62,35 @@ def login():
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
+    
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if request.method == "POST":
+        username = request.form.get("username")
+        if not username:
+            return redirect("/")
+
+        password = request.form.get("password")
+        if not password:
+            return redirect("/")
+
+        confirm = request.form.get("confirmation")
+        if not confirm:
+            return redirect("/")
+
+        if confirm != password:
+            return redirect("/")
+
+        usernames = db.execute("SELECT * FROM users WHERE username = ?", username)
+        if not usernames:
+            hashed_pwd = generate_password_hash(password)
+            db.execute("INSERT INTO users (username, hash) VALUES (?,?)", username, hashed_pwd)
+        else:
+            return redirect("/")
+        return redirect("/")
+    else:
+        return render_template("register.html")
+    
+# onde dá erro eu preciso criar alguma coisa pra acontecer ao invés de so redirecionar pra outro lugar, bad design pra krl
